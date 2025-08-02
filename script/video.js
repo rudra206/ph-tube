5ea7d826d2978c1c213d3046b77c88e3324ffdf5
rura206 <rudradipta34@gmail.com>
2025-08-02 19:33:47 +0600

    4

--- script/video.js
+++ script/video.js
@@ -1,4 +1,4 @@
-//1 -Fetch,Load and show Cateories on html
+//1 -Fetch,Load and show Categories on html
 
 
 //create loadCategories
@@ -64,28 +64,33 @@ const displayVideos = (videos) => {
         const card = document.createElement('div');
         card.classList = "card card-compact "
         card.innerHTML = `
-<figure>
+<figure class ="relative">
     <img
       src=${video.thumbnail}
       class="h-fill w-full object-cover"
       alt="Shoes" />
+      <span class ="absolute right-2 bottom-2 bg-black rounded p-1 text-white">
+      ${video.others.posted_date}</span>
   </figure>
   <div class="px-0 py-2 flex gap-2">
    <div>
-        <img class="h-10 w-10 rounded-full object-cover" src=${video.authors[0].profile_picture} />
+        <img class="h-10 w-10 rounded-full object-cover" 
+        src=${video.authors[0].profile_picture} />
    </div>
    <div>
     <h2 class ="font-bold">${video.title} </h2>
 <div class="flex items-center gap-2">
  <p class ="text-gray-400">${video.authors[0].profile_name}</p>
- <img class="w-5 " src="https://img.icons8.com/?size=96&id=D9RtvkuOe31p&format=png"/>
-</div>
+ 
+ 
+ ${video.authors[0].verified == true ? `<img class="w-5 " src="https://img.icons8.com/?size=96&id=D9RtvkuOe31p&format=png"/>
+</div>`: " "}
    
 
     <p> </p>
    </div>
   </div>
-`;
+`; ///*${video.authors[0].verified == true ? "true" : "false"}*/
         videoContainer.append(card);
     })
 
